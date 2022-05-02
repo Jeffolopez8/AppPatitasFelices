90ce16ba7be0caf0af35d596f31120ff3085862f
Jeffolopez8 <jeffolopez8@gmail.com>
2022-05-02 16:17:33 -0500

    Agregar archivos de proyecto.

---
+++ AppPatitasFelicesProyecto/AppPatitasFelicesProyecto/LoginPatitasFelicesaml.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPatitasFelicesProyecto
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPatitasFelicesaml : ContentPage
+    {
+        public LoginPatitasFelicesaml()
+        {
+            InitializeComponent();
+        }
+
+        void clickregistrarse()
+        {
+            lbl
+            label_xname.GestureRecognizers.Add(
+            new TapGestureRecognizer()
+            {
+                Command = new Command(() => {
+                    /* Handle the click here */
+                })
+            });
+
+        }
+        private void iniciarsesionLogin_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AppPatitasFelicesProyecto/AppPatitasFelicesProyecto/RegistroPatitasFelices.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPatitasFelicesProyecto
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistroPatitasFelices : ContentPage
+    {
+        public RegistroPatitasFelices()
+        {
+            InitializeComponent();
+        }
+    }
+}
